ae3363abafd794b93195f27122d2dd97cb06919b
Weikoi <huangzengrui@yahoo.com>
2022-01-06 18:55:48 +0800

    调整项目结构

---
+++ Titanic/demo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# *************************************************************
+# Copyright (c) Huatai Securities
+#
+# Author: Huang Zengrui <huangzengrui@htsc.com> 
+# CreateTime: 2022/1/5 15:42
+# DESC: 
+# *************************************************************
+
+import pandas as pd
+
+df = pd.read_csv("./data/train.csv")
+
+print(df)
---
+++ Titanic/model/model.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# *************************************************************
+# Copyright (c) Huatai Securities
+#
+# Author: Huang Zengrui <huangzengrui@htsc.com> 
+# CreateTime: 2022/1/5 14:40
+# DESC: 
+# *************************************************************
+
+class Model(object):
+
+    def __init__(self):
+        pass
+
