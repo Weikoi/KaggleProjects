74a2253f65e988f2de4e452e229266bb1aaebdad
Weikoi <huangzengrui@yahoo.com>
2022-01-13 15:12:11 +0800

    bugfix

--- Titanic/demo.py
+++ Titanic/demo.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # *************************************************************
-# Copyright (c) Huatai Securities
-#
 # Author: Huang Zengrui <huangzengrui@yahoo.com>
 # CreateTime: 2022/1/5 15:42
 # DESC: 
--- Titanic/model/model.py
+++ Titanic/model/model.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # *************************************************************
-# Copyright (c) Huatai Securities
-#
 # Author: Huang Zengrui <huangzengrui@yahoo.com>
 # CreateTime: 2022/1/5 14:40
 # DESC: 
